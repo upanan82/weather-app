ad787d54f19cd2e16d55c5ddf9407ab5fb8f37b8
Dmytro Onyshchenko <dmytro.onyshchenko@globallogic.com>
2018-06-25 11:40:48 +0300

    first commit

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { ApiComponent } from '../api/api.component';
+import { FormsModule } from '@angular/forms';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -8,7 +10,8 @@ describe('FormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ FormComponent, ApiComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
