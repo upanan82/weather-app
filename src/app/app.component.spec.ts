a5f0c7558297563440428b4c4b2b3c2fc75ebe5e
Dmytro Onyshchenko <dmytro.onyshchenko@globallogic.com>
2018-06-25 11:36:11 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to A-route!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Weather App');
   }));
 });
