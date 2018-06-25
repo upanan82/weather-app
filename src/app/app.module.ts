9f30ac7c6a60ac0372ad0a8cbeb1b74f0ff84665
Dmytro Onyshchenko <dmytro.onyshchenko@globallogic.com>
2018-06-25 11:23:28 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormModule } from './form/form.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormModule } from './form/form.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     FormModule
   ],
   providers: [],
