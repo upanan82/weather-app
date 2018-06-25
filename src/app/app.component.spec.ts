67934902a18c4991e02412acc9a3093a5b54c332
Dmytro Onyshchenko <dmytro.onyshchenko@globallogic.com>
2018-06-25 11:31:59 +0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,10 +15,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Weather App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Weather App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
