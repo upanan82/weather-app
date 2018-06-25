29bd54331e85063899336d5aedf2f1fd2a8dd403
Dmytro Onyshchenko <dmytro.onyshchenko@globallogic.com>
2018-06-25 13:05:08 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { AppService } from './app.service';
     FormModule,
     HttpModule
   ],
-  providers: [ AppService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
